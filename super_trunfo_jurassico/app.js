02ed883628a200a8087f7cc5feb8f60b78370b9c
JoseOnildo <joseodf@yahoo.com>
2023-12-06 23:13:46 -0300

    Update

--- super_trunfo_jurassico/app.js
+++ super_trunfo_jurassico/app.js
@@ -1,4 +1,4 @@
-console.clear();
+//console.clear();
 
 var cartas = [{nome:"Blue",
                imagem: "img/blue.png",
@@ -21,18 +21,17 @@ var cartas = [{nome:"Blue",
              {nome:"Indoraptor",
               imagem: "img/indoraptor.png",
               atributos: {Ataque: 9, Defesa: 4, Grito: 6}}];
-var cartaMaquina;
-var cartaJogador;
-var atributoMaquina;
-var atributoJogador;
+var cartaMaquina, cartaJogador, atributoMaquina, atributoJogador;
 var resultado = document.getElementById("resultado");
 var divCartaMaquina = document.getElementById("carta-maquina");
 var divCartaJogador = document.getElementById("carta-jogador");
+var moldura = '<img src="img/card-super-trunfo.png" style=" width: inherit; height: inherit; position: absolute;">';
+var tagHTML = "<div id='opcoes' class= 'carta-status'>";
 
 function sortearCarta() {
-  console.clear();
+  //console.clear();
   resultado.innerHTML = "";
-  divCartaMaquina.innerHTML = '<img src="img/card-super-trunfo.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaMaquina.innerHTML = moldura;
   divCartaMaquina.style.backgroundImage = "";
   var numeroCarta = parseInt(Math.random() * 7);
   cartaMaquina = cartas[numeroCarta]; 
@@ -52,9 +51,7 @@ function sortearCarta() {
 }
 
 function exibirCartaJogador(){
-  var nome = "<p class='carta-subtitle'>" + cartaJogador.nome + "</p>";
-  var moldura = '<img src="img/card-super-trunfo.png" style=" width: inherit; height: inherit; position: absolute;">';
-  var tagHTML = "<div id='opcoes' class= 'carta-status'>";
+  var nome = "<p class='carta-subtitle'>" + cartaJogador.nome + "</p>";  
   var radioOpcoes = "";
   for (var atributoJ in cartaJogador.atributos){
     radioOpcoes += "<input type='radio' name='atributoJ' value='" + atributoJ + "'>" + atributoJ + "<br>";
@@ -64,9 +61,7 @@ function exibirCartaJogador(){
 }
 
 function exibirCartaMaquina(){
-  var nome = "<p class='carta-subtitle'>" + cartaMaquina.nome + "</p>";
-  var moldura = '<img src="img/card-super-trunfo.png" style=" width: inherit; height: inherit; position: absolute;">';
-  var tagHTML = "<div id='opcoes' class= 'carta-status'>";
+  var nome = "<p class='carta-subtitle'>" + cartaMaquina.nome + "</p>";  
   var radioOpcoes = "";
   for (var atributoM in cartaMaquina.atributos){
     radioOpcoes += "<input type='radio' name='atributoM' value='" + atributoM + "'>" + atributoM + " " + cartaMaquina.atributos[atributoM] + "<br>";
@@ -76,45 +71,24 @@ function exibirCartaMaquina(){
 } 
 
 function jogar(){
-  atributoJogador = obtemAtributoJogador();
-     
+  atributoJogador = obtemAtributoJogador();     
   if (atributoJogador == undefined) {
-    document.getElementById("btnSortear").disabled = true;
-    document.getElementById("btnJogar").disabled = false;
-    resultado.innerHTML = "<p class='resultado-final'>ERRO!! Poder não selecionado</p>";
-    delay = 1000; setTimeout(function() {resultado.innerHTML = "";}, delay);}
-    
-  else{    
-    document.getElementById("btnSortear").disabled = false;
-    document.getElementById("btnJogar").disabled = true;
-    
-    exibirCartaMaquina();
-    marcaAtributoMaquina();
-    exibirCartaJogadorCompleta();
-    
-    
-    var valorCartaMaquina = cartaMaquina.atributos[atributoMaquina];
-    var valorCartaJogador = cartaJogador.atributos[atributoJogador];
-    
-        
-    if (valorCartaMaquina > valorCartaJogador){resultado.innerHTML = "<p class='resultado-final'>Maquina ganhou!!</p>"}
-    else if (valorCartaMaquina < valorCartaJogador){resultado.innerHTML = "<p class='resultado-final'>Parabens!! Você ganhou!!</p>"}
-    else {resultado.innerHTML = "<p class='resultado-final'>Empate!!</p>"}
-    
-        
-    }
-  console.log("Carta Jogador: " + cartaJogador.nome + " - " + atributoJogador + ": " + valorCartaJogador);
-  console.log("Carta Maquina: " + cartaMaquina.nome + " - " + atributoMaquina + ": " + valorCartaMaquina);
-  }
+    //document.getElementById("btnSortear").disabled = true;
+    //document.getElementById("btnJogar").disabled = false;
+    //resultado.innerHTML = "<p class='resultado-final'>ERRO!! Poder não selecionado</p>";
+    //alert(atributoJogador);
+    //delay = 1000; setTimeout(function() {resultado.innerHTML = "";}, delay);
+  } 
+}
 
- function obtemAtributoJogador(){
+function obtemAtributoJogador(){
    var radioAtributos = document.getElementsByName("atributoJ");
    for (var i = 0; i < radioAtributos.length; i++) {
      if (radioAtributos[i].checked == true) {
        return radioAtributos[i].value;
      }
    }
- }
+}
 
 function marcaAtributoMaquina(){
    var radioAtributos = document.getElementsByName("atributoM");
@@ -127,8 +101,6 @@ function marcaAtributoMaquina(){
 
 function exibirCartaJogadorCompleta(){
   var nome = "<p class='carta-subtitle'>" + cartaJogador.nome + "</p>";
-  var moldura = '<img src="img/card-super-trunfo.png" style=" width: inherit; height: inherit; position: absolute;">';
-  var tagHTML = "<div id='opcoes' class= 'carta-status'>";
   var radioOpcoes = "";
   for (var atributoJ in cartaJogador.atributos){
     radioOpcoes += "<input type='radio' name='atributoJ' value='" + atributoJ + "'>" + atributoJ + " " + cartaJogador.atributos[atributoJ] + "<br>";
